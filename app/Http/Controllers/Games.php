aad9959c62832765f20ef62e7772014a555e7ea7
GUSTAV HENRIQUE <gomessenag@gmail.com>
2023-09-28 01:18:04 -0300

    backup 1

---
+++ app/Http/Controllers/Games.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Games extends Controller
+{
+    public function pagina_incial()
+    {
+        return view('welcome');
+    }
+}
+
+
+
+
+
+
---
+++ database/migrations/2023_09_28_032945_create_jogos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jogos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo');
+            $table->string('imagem');
+            $table->date('data_lançamento');
+            $table->string('trailer_youtube');
+            $table->string('link_jogo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jogos');
+    }
+};
